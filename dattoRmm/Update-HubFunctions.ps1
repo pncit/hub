7cb18219f385fbecc048beddc94ee1465f9fe2cf
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2019-06-09 22:52:46 -0800

    added option to source production code for full tests

--- dattoRmm/Update-HubFunctions.ps1
+++ dattoRmm/Update-HubFunctions.ps1
@@ -1,24 +1,47 @@
-function Update-Functions {
+function Update-HubFunctions {
     <#
     .SYNOPSIS 
-    Updates all system-defined functions
+    Updates all system-defined functions and variables
 
     .DESCRIPTION
     When developing and testing code, it can be helpful to be able to quickly update all functions within an active session 
     to absorb any changes made to the function definitions. This function removes the hubFunctions module if necessary, 
     rebuilds the code, and imports the refreshed module.
+
+    .PARAMETER prod
+    If omitted, local files will be sourced. If true, function will pull from production server specified in /dattoRmm/protected/Get-HubFunctions.ps1
+
+    .EXAMPLE
+    . .\dattoRmm\Update-Hubfunctions.ps1
+    update-HubFunctions
+
+    .EXAMPLE
+    (from admin cmd)
+    psexec -i -s powershell.exe
+    . .\dattoRmm\Update-Hubfunctions.ps1
+    update-HubFunctions -prod
     #>
 
-    $module = Get-Module | Where-Object { $_.name -eq "hubFunctions" }
-    if ( $null -ne $module ) {
-        Remove-Module hubFunctions
+    param(
+        [parameter(Mandatory=$false)]
+        [Switch]$prod
+    )
+
+    if ( $prod -eq $true ) {
+        $env:udf_30 = Get-Content -Path ".\dattoRmm\protected\encryptionKey.AES"
+        . .\dattoRmm\protected\Get-HubFunctions.ps1
+        Remove-Item Env:\udf_30
+    } else {
+        $module = Get-Module | Where-Object { $_.name -eq "hubFunctions" }
+        if ( $null -ne $module ) {
+            Remove-Module hubFunctions
+        }
+        . "./dattoRmm/Build-HubFunctions.ps1"
+        Import-Module ".\dattoRmm\protected\hubFunctions.psm1" -Global
+        . .\dattoRmm\protected\hubFunctionsConfig.ps1
     }
-    . "./dattoRmm/Build-HubFunctions.ps1"
-    Import-Module ".\dattoRmm\protected\hubFunctions.psm1" -Global
 
     $module = Get-Module | Where-Object { $_.name -eq "hubFunctions" }
     Write-Host "Successfully imported $($module.ExportedCommands.Values.Count) functions:"
     $module.ExportedCommands.Values
-
-    . .\dattoRmm\protected\hubFunctionsConfig.ps1
 }

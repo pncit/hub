229b3e3b45720fe1747098d54d7cf40086872a0a
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2019-06-10 05:11:36 -0800

    added new variables

--- dattoRmm/templates/hubFunctionsConfig.ps1
+++ dattoRmm/templates/hubFunctionsConfig.ps1
@@ -8,25 +8,45 @@ $cwmCompany = "mycompany"
 $cwmApiRegion = "xx"
 $cwmApiPublicKey = "0000000000000000"
 $cwmApiPrivateKey = "0000000000000000"
+$cwmServiceBoardId = 0
+$cwmProjectBoardId = 0
+$cwmServiceTicketStatusClosed = 0
+$cwmServiceTicketStatusOpen = 0
+$cwmProjectTicketStatusClosed = 0
+$cwmProjectTicketStatusOpen = 0
+$cwmMemberId = 0
+$cwmWorkRoleId = 0
 
-#microsoft teams settings
-$msTeamsWebhook = "https://outlook.office.com/webhook/00000000-0000-0000-0000-000000000000@00000000-0000-0000-0000-000000000000/IncomingWebhook/00000000000000000000000000000000/00000000-0000-0000-0000-000000000000"
+#office365 settings
+$o365Account = "sender@conosto.com"
+$o365Password = 'password'
+$msTeamsWebhook = "https://outlook.office.com/webhook/00000000-0000-0000-0000-000000000000000000000-0000-0000-0000-000000000000/IncomingWebhook/00000000000000000000000000000000/00000000-0000-0000-0000-000000000000"
 
-
+#office365
 ################################################################
 # below this point all settings are dynamic
 
-$Global:dattoRmmApiAccessParams = @{
+$global:dattoRmmApiAccessParams = @{
     apiUrl = $dattoRmmApiUrl
     accessKey = $dattoRmmAccessKey
     secretKey = $dattoRmmSecretKey
 }
 
-$Global:cwmApiAccessParams = @{
-    company = $cwmCompany
-    apiRegion = $cwmApiRegion
-    publicKey = $cwmApiPublicKey
-    privateKey = $cwmApiPrivateKey
-}
+$global:cwmApiUrl = Get-CwmApiUrl -apiRegion $cwmApiRegion -company $cwmCompany
+$global:cwmApiAuthString = New-CwmApiAuthString -company $cwmCompany -publicKey $cwmApiPublicKey -privateKey $cwmApiPrivateKey
+$global:cwmServiceBoardId = $cwmServiceBoardId
+$global:cwmProjectBoardId = $cwmProjectBoardId
+$global:cwmServiceTicketStatusClosed = $cwmServiceTicketStatusClosed
+$global:cwmProjectTicketStatusClosed = $cwmProjectTicketStatusClosed
+$global:cwmServiceTicketStatusOpen = $cwmServiceTicketStatusOpen
+$global:cwmProjectTicketStatusOpen = $cwmProjectTicketStatusOpen
+$global:cwmMemberId = $cwmMemberId
+$global:cwmWorkRoleId = $cwmWorkRoleId
 
-$Global:msTeamsWebhook = $msTeamsWebhook
+$o365PasswordSecure = ConvertTo-SecureString $o365Password -AsPlainText -Force
+$o365Credential = New-Object System.Management.Automation.PSCredential( $o365Account , $o365PasswordSecure )
+$global:o365MailParams = @{ 
+    From = $o365Account
+    Credential = $o365Credential
+}
+$global:msTeamsWebhook = $msTeamsWebhook

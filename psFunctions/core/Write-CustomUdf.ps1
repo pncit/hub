66b3a597f638d361ab5e88ba84f3446015d677c0
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2019-06-09 22:06:53 -0800

    Create Write-CustomUdf.ps1

---
+++ psFunctions/core/Write-CustomUdf.ps1
@@ -0,0 +1,31 @@
+function Write-CustomUdf {
+    <#
+    .SYNOPSIS 
+    Writes a value to a Datto RMM Custom UDF
+
+    .DESCRIPTION
+    Writes a specified value to a specified Datto RMM Custom UDF via the registry
+
+    .PARAMETER Udf
+    UDF to write to (1-30)
+
+    .PARAMETER Value
+    Value to write
+
+    .EXAMPLE
+    Write-CustomUdf -customUdf 22 -value 'Complete'
+    #>
+    param (
+        [parameter(Mandatory=$true)]
+        [ValidateRange(1,30)]
+        [int32]$udf,
+        
+        [parameter(Mandatory=$true)]
+        [validateNotNullorEmpty()]
+        [string]
+        $Value
+    )
+
+    Set-ItemProperty -Path HKLM:\SOFTWARE\CentraStage -Name Custom$udf  -Value $Value
+
+}

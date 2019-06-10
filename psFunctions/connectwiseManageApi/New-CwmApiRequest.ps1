73d611016487b0f73915397a2758a3915e11ec7a
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2019-06-10 05:01:56 -0800

    simplified calling options
    was having trouble with poorly defined parameter sets. reduced function to one call method for which url and access token are required.

--- psFunctions/connectwiseManageApi/New-CwmApiRequest.ps1
+++ psFunctions/connectwiseManageApi/New-CwmApiRequest.ps1
@@ -3,138 +3,47 @@ function New-CwmApiRequest {
     .SYNOPSIS 
     Performs a query against the Connectwise Manage API
 
-    .DESCRIPTION
-    Connectwise changes their code bases regularly. This function generates an api url either for a specified code base or returns the most updated
-    code base available to your company.
-
-    .PARAMETER company
-    Your company id
-
     .PARAMETER endpoint
 
     .PARAMETER apiUrl
 
-    .PARAMETER apiRegion
-    API region. Allowable values are:
-    au
-    eu
-    ​na
-
-    .PARAMETER codebase
-    To avoid breaking changes as the API develops, you can specify a specific code base (e.g. v4_6_release/). If this is not specified, the function
-    will return the URL for the most current code base. This is not needed if specifying company.
-
-    codebase value must end in a forward slash "/"
-
     .PARAMETER authString
-    
-    .PARAMETER publicKey
-
-    .PARAMETER privateKey
 
     .OUTPUTS
     API response
 
-    .EXAMPLE
-    $contacts = New-CwmApiRequest -company "mycompany" -endpoint "company/contacts" -apiMethod "get" -apiRegion "na" -publicKey "xxxx" -privateKey "yyyy"
-    This will return all contacts using the most recent api codebase and public key + private key to authenticate
-     .EXAMPLE
-    $contacts = New-CwmApiRequest -company "mycompany" -endpoint "company/contacts" -apiMethod "get" -apiRegion "na" -codebase "2017_4" -authString "Basic 1234" 
-    This will return all contacts using the 2017_4 api codebase and a pre-defined authorization string to authenticate
-    
-     .EXAMPLE
-    $contacts = New-CwmApiRequest -company "mycompany" -endpoint "company/contacts" -apiMethod "get" -apiUrl "https://api-na.myconnectwise.net/v2019_3/apis/3.0/" -publicKey "xxxx" -privateKey "yyyy"
-    This will return all contacts using a pre-defined api url and public key + private key to authenticate
-    
      .EXAMPLE
     $contacts = New-CwmApiRequest -endpoint "company/contacts" -apiMethod "get" -apiUrl "https://api-na.myconnectwise.net/v2019_3/apis/3.0/" -authString "Basic XXX" 
-    This will return all contacts using a pre-defined api url and a pre-defined authorization string to authenticate
 
     .NOTES
     #>
     [CmdletBinding()]
 	param
 	(
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, -authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "+apiurl, -authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, +authstring, -codebase")]
-        [validateNotNullorEmpty()]
-        [string]$company,
-        
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, -authstring")]
-        [parameter(Mandatory=$false,ParameterSetName = "+apiurl, -authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, +authstring, -codebase")]
-        [parameter(Mandatory=$false,ParameterSetName = "+apiurl, +authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, +authstring, +codebase")]
+        [parameter(Mandatory=$true)]
         [validateNotNullorEmpty()]
         [string]$endpoint,
 
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, -authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "+apiurl, -authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, +authstring, -codebase")]
-        [parameter(Mandatory=$true,ParameterSetName = "+apiurl, +authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, +authstring, +codebase")]
+        [parameter(Mandatory=$true)]
         [validateNotNullorEmpty()]
         [string]$apiMethod,
 
-        [parameter(Mandatory=$false,ParameterSetName = "-apiurl, -authstring")]
-        [parameter(Mandatory=$false,ParameterSetName = "+apiurl, -authstring")]
-        [parameter(Mandatory=$false,ParameterSetName = "-apiurl, +authstring, -codebase")]
-        [parameter(Mandatory=$false,ParameterSetName = "+apiurl, +authstring")]
-        [parameter(Mandatory=$false,ParameterSetName = "-apiurl, +authstring, +codebase")]
+        [parameter(Mandatory=$false)]
         [validateNotNullorEmpty()]
         [string]$apiRequestBody,
 
-        [parameter(Mandatory=$true,ParameterSetName = "+apiurl, -authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "+apiurl, +authstring")]
+        [parameter(Mandatory=$true)]
         [validateNotNullorEmpty()]
         [string]$apiUrl,
 
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, -authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, +authstring, -codebase")]
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, +authstring, +codebase")]
-        [ValidateSet("au","eu","na")]
-        [string]$apiRegion,
-
-        [parameter(Mandatory=$false,ParameterSetName = "-apiurl, -authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, +authstring, +codebase")]
+        [parameter(Mandatory=$true)]
         [validateNotNullorEmpty()]
-        [string]$codebase,
-
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, +authstring, -codebase")]
-        [parameter(Mandatory=$true,ParameterSetName = "+apiurl, +authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, +authstring, +codebase")]
-        [validateNotNullorEmpty()]
-        [string]$authString,
-
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, -authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "+apiurl, -authstring")]
-        [validateNotNullorEmpty()]
-        [string]$publicKey,
-
-        [parameter(Mandatory=$true,ParameterSetName = "-apiurl, -authstring")]
-        [parameter(Mandatory=$true,ParameterSetName = "+apiurl, -authstring")]
-        [validateNotNullorEmpty()]
-        [string]$privateKey
+        [string]$authString
     )
 
     $errorAction = $PSBoundParameters["ErrorAction"]
     if(-not $errorAction){
         $errorAction = $ErrorActionPreference
-    }
-
-    #generate api url if not specified
-    if ( ( $PSBoundParameters.ContainsKey( 'apiUrl')  ) -eq $false ) {
-        if ( ( $PSBoundParameters.ContainsKey( 'codebase')  ) -eq $false ) {
-            $apiUrl = Get-CwmApiUrl -apiRegion $apiRegion -company $company
-        } else {
-            $apiUrl = Get-CwmApiUrl -apiRegion $apiRegion -codebase $codebase
-        }
-    }
-
-    #generate auth string if not specified
-    if ( ( $PSBoundParameters.ContainsKey( 'authString')  ) -eq $false ) {
-        $authString = New-CwmApiAuthString -company $company -publicKey $publicKey -privateKey $privateKey
     }
 
     #set the parameters for the request

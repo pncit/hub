acbb9e2db75dd657f76f3c0dc3967a3425330d3a
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2019-06-20 00:06:27 -0800

    leverage hubFunctions to set the key

--- dattoRmm/Set-EncryptionKeyUdf.ps1
+++ dattoRmm/Set-EncryptionKeyUdf.ps1
@@ -1,17 +1,12 @@
-# load config so we have $dattoRmmApiAccessParams
-. .\dattoRmm\protected\hubFunctionsConfig.ps1
-
 # import functions so we have the api functions
 . .\dattoRmm\Update-HubFunctions.ps1
+Update-HubFunctions
+
+# load config so we have $dattoRmmApiAccessParams
+. .\dattoRmm\protected\hubFunctionsConfig.ps1
 
 # get the encryption key value
 $encryptionKey = Get-Content '.\dattoRmm\protected\encryptionKey.AES'
 
 # record the encryption key in udf_30
-$apiAccessToken = New-DattoRmmApiAccessToken -apiUrl $dattoRmmApiAccessParams['apiUrl'] -accessKey $dattoRmmApiAccessParams['accessKey'] -secretKey $dattoRmmApiAccessParams['secretKey']
-
-$fastAccessParams = @{
-    apiUrl = $dattoRmmApiAccessParams['apiUrl']
-    apiAccessToken = $apiAccessToken
-}
-Set-DeviceUdf -dattoRmmApiAccessParams $fastAccessParams -udf 30 -value $encryptionKey
+Set-DeviceUdf -dattoRmmApiAccessParams $dattoRmmApiAccessParams -udf 30 -value $encryptionKey

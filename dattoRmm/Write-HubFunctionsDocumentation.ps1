fc66a94b25cea7d80f8868cd1fbdee46effe9c92
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2019-06-28 15:37:52 -0800

    create one md file per psFunctions subfolder
    this balances between too many md files to parse through and one md file too big to parse through.

--- dattoRmm/Write-HubFunctionsDocumentation.ps1
+++ dattoRmm/Write-HubFunctionsDocumentation.ps1
@@ -2,11 +2,43 @@
 #utilizes platyPS
 #https://github.com/PowerShell/platyPS
 
+function Repair-File {
+    param( $file )
+    if ( (Get-Content $file -Encoding byte -TotalCount 1) -ne 239 ) {
+        [char]0xEF + [char]0xBB + [char]0xBF + ( Get-Content $file -Raw ) | Set-Content $file
+    }
+}
+
+function Write-Documentation {
+    param( $module ) 
+    #combine all function definitions into one file
+    $tempFolder = "$env:temp\$module"
+    $tempFile = "$tempFolder\$module.psm1"
+    mkdir $tempFolder
+    Get-ChildItem ".\psFunctions\$module" *ps1 -Recurse | Get-Content | Set-Content $tempFile -Force
+    # import functions
+    Import-Module $tempFile
+    Remove-Item -Path $tempFile
+    New-MarkdownHelp -Module $module -OutputFolder "$tempFolder" -Force
+    Get-ChildItem "$tempFolder" *-*md | Get-Content | Set-Content ".\docs\psFunctionDocumentation\$module.md" -Force
+    Remove-Item -Path $tempFolder -Recurse -Force
+}
+
 Install-Module -Name platyPS -Scope CurrentUser
 Import-Module platyPS
-. .\dattoRmm\Update-HubFunctions.ps1
-Update-HubFunctions
-Remove-Item -path '.\docs\*' -Include '*.md' -Exclude 'installationInstructions.md'
-New-MarkdownHelp -Module hubFunctions -OutputFolder '.\docs' -Force
-Get-ChildItem '.\docs' *-*md | Get-Content | Set-Content '.\docs\functionDocumentation.md' -Force
-Remove-Item -path '.\docs\*' -Include '*.md' -Exclude 'functionDocumentation.md'
+
+#fix encoding on encryption files if necessary
+Repair-File -file ".\psFunctions\cryptography\Protect-File.ps1"
+Repair-File -file ".\psFunctions\cryptography\Unprotect-File.ps1"
+Repair-File -file ".\psFunctions\cryptography\New-CryptographyKey.ps1"
+
+#clear existing documentation
+Remove-Item -path '.\docs\psFunctionDocumentation\*' -Include '*.md'
+
+#write new documentation
+Write-Documentation "connectwiseManageApi"
+Write-Documentation "cryptography"
+Write-Documentation "dattoRmm"
+Write-Documentation "dattoRmmApi"
+Write-Documentation "o365"
+Write-Documentation "windows"

1d63a7131dd2e7b8b88ede70fbb7789657b05b2b
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2019-07-01 14:49:02 -0800

    add mit license to crypto functions

--- psFunctions/cryptography/Unprotect-File.ps1
+++ psFunctions/cryptography/Unprotect-File.ps1
@@ -47,6 +47,27 @@ This example decrypts all of the files under the C:\Files directory using the ke
 Author: Tyler Siegrist
 Date: 9/22/2017
 Downloaded from https://gallery.technet.microsoft.com/scriptcenter/EncryptDecrypt-files-use-65e7ae5d on 3/8/2019
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 #>
 [CmdletBinding(DefaultParameterSetName='SecureString')]
 [OutputType([System.IO.FileInfo[]])]

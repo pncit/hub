d8f8fb3eabb8b00fd2c1473640a90f74aa6f7fa7
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2019-07-01 11:53:08 -0800

    fix header markdown

--- dattoRmm/Write-HubFunctionsDocumentation.ps1
+++ dattoRmm/Write-HubFunctionsDocumentation.ps1
@@ -25,8 +25,8 @@ function Write-Documentation {
     New-MarkdownHelp -Module $module -OutputFolder "$tempFolder" -Force -NoMetadata
     Remove-Module $module -ErrorAction SilentlyContinue
     Get-ChildItem "$tempFolder" *-*md | Get-Content | Set-Content $tempDoc -Force
-    $functionList = (((Get-Content $tempDoc) | Where-Object { $_ -match "^# " } ) -replace "# ","") | ForEach-Object { "##[$_](#$_)`r`n`r`n" }
-    "Functions:`r`n$functionList" | Set-Content $docFile
+    $functionList = (((Get-Content $tempDoc) | Where-Object { $_ -match "^# " } ) -replace "# ","") | ForEach-Object { "## [$_](#$_)`r`n`r`n" }
+    "# Functions:`r`n$functionList" | Set-Content $docFile
     (Get-Content $tempDoc) -replace "^# ","`r`n&nbsp;`r`n&nbsp;`r`n&nbsp;`r`n# " | Add-Content $docFile
     Remove-Item -Path $tempFolder -Recurse -Force
 }

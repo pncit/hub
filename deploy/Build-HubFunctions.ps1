23205ccd24e607c6a6759b581cd8e195f2504913
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2020-08-20 10:07:39 -0800

    Update Build-HubFunctions.ps1
    added some ui information

--- deploy/Build-HubFunctions.ps1
+++ deploy/Build-HubFunctions.ps1
@@ -10,9 +10,13 @@ Repair-File -file ".\psFunctions\cryptography\Protect-File.ps1"
 Repair-File -file ".\psFunctions\cryptography\Unprotect-File.ps1"
 Repair-File -file ".\psFunctions\cryptography\New-CryptographyKey.ps1"
 
+Write-Host "Building hub functions psm1 file..."
 #combine all function definitions into one file
 Get-ChildItem ".\psFunctions" *ps1 -Recurse | Get-Content | Set-Content ".\deploy\hubFunctions.psm1" -Force
 
 #compress file and remove original
+Write-Host "Creating compressed hub functions file..."
 Compress-Archive -Path ".\deploy\hubFunctions.psm1" -CompressionLevel Optimal -DestinationPath ".\hubFunctions.zip" -Force
+Write-Host "Removing hub functions psm1 file..."
 Remove-Item -LiteralPath ".\deploy\hubFunctions.psm1"
+Write-Host "Completed building hub functions."
